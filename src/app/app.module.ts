72a77fa286be1f1300f0d5bac0f13a2cdf0cbadc
JANAT <janat@JANATs-iMac.domain>
2017-10-20 01:15:17 -0600

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { SampleModule } from 'angular4-highcharts';
+import { ChartModule } from 'angular-highcharts';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SampleModule.forRoot(),
+    ChartModule // add ChartModule to your imports
   ],
   providers: [],
   bootstrap: [AppComponent]
